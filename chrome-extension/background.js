792409fa0220b399ccb0740559fd899f8583739d
Yaniv Kalsky <yanivka@gmail.com>
2017-12-22 22:25:38 -0800

    Cleanups

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,7 +3,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
   if (~tab.url.indexOf('www.facebook.com') && changeInfo.status=='complete') {
     chrome.pageAction.show(tabId);
-    console.log('on updated');
+    //console.log('on updated');
     chrome.tabs.sendMessage(tabId, {type: 'getDoc'}, function (doc) {
         //console.log(doc);
     });
