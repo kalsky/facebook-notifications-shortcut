5e1e113356f4d48503268bf85fcce2afe2db987a
Yaniv Kalsky <yanivka@gmail.com>
2017-12-22 22:25:26 -0800

    Firefox add-on

---
+++ firefox-add-on/background.js
@@ -0,0 +1,18 @@
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+  if (~tab.url.indexOf('www.facebook.com') && changeInfo.status=='complete') {
+    browser.pageAction.show(tabId);
+    //console.log('on updated');
+    browser.tabs.sendMessage(tabId, {type: 'getDoc'}, function (doc) {
+        //console.log('after sending');
+    });
+  }
+
+});
+
+
+browser.pageAction.onClicked.addListener((tab) => {
+  //console.log('onClicked');
+  browser.tabs.executeScript(null,{file:"myscript.js"});
+});
